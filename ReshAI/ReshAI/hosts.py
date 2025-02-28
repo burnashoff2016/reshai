a7af55cb3826e35669f9d82680a4edd22dbb6a14
Артем <burnashoff2016@yandex.ru>
2025-02-28 11:51:21 +0300

    0.8.3

--- ReshAI/ReshAI/hosts.py
+++ ReshAI/ReshAI/hosts.py
@@ -2,8 +2,8 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     # Основной домен – mainapp
-    host(r'www', 'ReshAI.urls_app', name='www'),
-    host(r'^$', 'ReshAI.urls_app', name='root'),  # для голого resh-ai.ru, если нужно
+    host(r'www', 'ReshAI.urls_main', name='www'),
+    host(r'^$', 'ReshAI.urls_main', name='root'),  # для голого resh-ai.ru, если нужно
     # Поддомен – chatapp
-    host(r'app', 'ReshAI.urls_main', name='app'),
+    host(r'app', 'ReshAI.urls_app', name='app'),
 )
